419f1650e10765f05fdd06cf2fe1bf7f77041916
Coveting Aphid <tadiwachawanda@gmail.com>
2024-01-08 19:06:51 +0200

    ...

---
+++ script.js
@@ -0,0 +1,14 @@
+// document.addEventListener('DOMContentLoaded',function(){
+//     const display = document.querySelector('.display');
+//     const buttons = document.querySelectorAll('button');
+
+//     buttons.forEach(button =>{
+//         button.addEventListener('click',function(){
+//             display.textContent = button.textContent
+//         })
+//     })
+// })
+
+const display = document.querySelector('.display');
+let value = display.innerHTML
+
