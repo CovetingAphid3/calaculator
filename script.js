6a951fb05e8c080d241f2fdab9d14805f79c1e62
root <root@LAPTOP-36G6KV7C.>
2024-01-09 13:16:46 +0200

    Finishded calculator app

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-// document.addEventListener('DOMContentLoaded',function(){
-//     const display = document.querySelector('.display');
-//     const buttons = document.querySelectorAll('button');
+// Function to update the display value
+function updateDisplay(value) {
+    document.querySelector(".display input[name='display']").value = value;
+}
 
-//     buttons.forEach(button =>{
-//         button.addEventListener('click',function(){
-//             display.textContent = button.textContent
-//         })
-//     })
-// })
+// Function to handle button clicks
+function buttonClick(input) {
+    var displayValue = document.querySelector(".display input[name='display']").value;
 
-const display = document.querySelector('.display');
-let value = display.innerHTML
+    if (input === 'AC') {
+        updateDisplay('');
+    } else if (input === 'DE') {
+        updateDisplay(displayValue.slice(0, -1));
+    } else if (input === '=') {
+        updateDisplay(addbits(displayValue));
+    } else {
+        updateDisplay(displayValue + input);
+    }
+}
 
+let numbers = document.querySelectorAll('.operator,.operator1')
+
+numbers.forEach(function(number){
+    number.addEventListener('click',function(){
+        
+        buttonClick(number.value);
+        
+    })
+})
+
+
+function addbits(displayValue) {
+    try {
+        return math.evaluate(displayValue);
+    } catch (error) {
+        // Handle any potential evaluation errors
+        alert('Error: ' + error.message);
+        return 0;
+    }
+}
